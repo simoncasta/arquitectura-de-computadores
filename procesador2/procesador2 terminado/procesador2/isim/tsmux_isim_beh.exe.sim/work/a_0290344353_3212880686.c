921bf1c87589d63c976acef56a9ebb576c5fe0ab
simoncasta <s-casta126@hotmail.com>
2017-04-18 19:10:55 -0500

    Add files via upload

---
+++ procesador2/procesador2 terminado/procesador2/isim/tsmux_isim_beh.exe.sim/work/a_0290344353_3212880686.c
@@ -0,0 +1,118 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/Martinez/Downloads/procesador/mux.vhd";
+
+
+
+static void work_a_0290344353_3212880686_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    unsigned char t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+
+LAB0:    xsi_set_current_line(46, ng0);
+    t1 = (t0 + 1672U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)3);
+    if (t4 != 0)
+        goto LAB2;
+
+LAB4:    xsi_set_current_line(49, ng0);
+    t1 = (t0 + 1352U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)2);
+    if (t4 != 0)
+        goto LAB5;
+
+LAB7:    xsi_set_current_line(52, ng0);
+    t1 = (t0 + 1352U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)3);
+    if (t4 != 0)
+        goto LAB8;
+
+LAB10:
+LAB9:
+LAB6:
+LAB3:    t1 = (t0 + 3152);
+    *((int *)t1) = 1;
+
+LAB1:    return;
+LAB2:    xsi_set_current_line(47, ng0);
+    t1 = xsi_get_transient_memory(32U);
+    memset(t1, 0, 32U);
+    t5 = t1;
+    memset(t5, (unsigned char)2, 32U);
+    t6 = (t0 + 3232);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    t9 = (t8 + 56U);
+    t10 = *((char **)t9);
+    memcpy(t10, t1, 32U);
+    xsi_driver_first_trans_fast_port(t6);
+    goto LAB3;
+
+LAB5:    xsi_set_current_line(50, ng0);
+    t1 = (t0 + 1032U);
+    t5 = *((char **)t1);
+    t1 = (t0 + 3232);
+    t6 = (t1 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t5, 32U);
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB6;
+
+LAB8:    xsi_set_current_line(53, ng0);
+    t1 = (t0 + 1192U);
+    t5 = *((char **)t1);
+    t1 = (t0 + 3232);
+    t6 = (t1 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t5, 32U);
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB9;
+
+}
+
+
+extern void work_a_0290344353_3212880686_init()
+{
+	static char *pe[] = {(void *)work_a_0290344353_3212880686_p_0};
+	xsi_register_didat("work_a_0290344353_3212880686", "isim/tsmux_isim_beh.exe.sim/work/a_0290344353_3212880686.didat");
+	xsi_register_executes(pe);
+}
